a630a25ffcc136acd9035b6b4d981c09ba632008
AndrewK <43419144+AndrewKuekam17@users.noreply.github.com>
2020-04-07 19:08:51 -0400

    Add files via upload

---
+++ FactorialDoWhileAndrew/FactorialDoWhileAndrew/FactorialForm.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace FactorialDoWhileAndrew
+{
+    partial class frmFactorialDoWhle
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNumber = new System.Windows.Forms.Label();
+            this.BtnStart = new System.Windows.Forms.Button();
+            this.lblMultiply = new System.Windows.Forms.Label();
+            this.txtNumber = new System.Windows.Forms.TextBox();
+            this.lblAnswer = new System.Windows.Forms.Label();
+            this.lstFactorialNumbers = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // lblNumber
+            // 
+            this.lblNumber.AutoSize = true;
+            this.lblNumber.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumber.Location = new System.Drawing.Point(97, 87);
+            this.lblNumber.Name = "lblNumber";
+            this.lblNumber.Size = new System.Drawing.Size(179, 24);
+            this.lblNumber.TabIndex = 0;
+            this.lblNumber.Text = "Enter the number:";
+            // 
+            // BtnStart
+            // 
+            this.BtnStart.BackColor = System.Drawing.Color.Yellow;
+            this.BtnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnStart.Location = new System.Drawing.Point(176, 148);
+            this.BtnStart.Name = "BtnStart";
+            this.BtnStart.Size = new System.Drawing.Size(171, 52);
+            this.BtnStart.TabIndex = 1;
+            this.BtnStart.Text = "Start";
+            this.BtnStart.UseVisualStyleBackColor = false;
+            this.BtnStart.Click += new System.EventHandler(this.BtnStart_Click);
+            // 
+            // lblMultiply
+            // 
+            this.lblMultiply.AutoSize = true;
+            this.lblMultiply.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblMultiply.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMultiply.Location = new System.Drawing.Point(97, 249);
+            this.lblMultiply.Name = "lblMultiply";
+            this.lblMultiply.Size = new System.Drawing.Size(382, 24);
+            this.lblMultiply.TabIndex = 2;
+            this.lblMultiply.Text = "Multiply the following numbers together:";
+            // 
+            // txtNumber
+            // 
+            this.txtNumber.Location = new System.Drawing.Point(282, 92);
+            this.txtNumber.Name = "txtNumber";
+            this.txtNumber.Size = new System.Drawing.Size(100, 20);
+            this.txtNumber.TabIndex = 4;
+            // 
+            // lblAnswer
+            // 
+            this.lblAnswer.AutoSize = true;
+            this.lblAnswer.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblAnswer.Location = new System.Drawing.Point(494, 388);
+            this.lblAnswer.Name = "lblAnswer";
+            this.lblAnswer.Size = new System.Drawing.Size(0, 13);
+            this.lblAnswer.TabIndex = 6;
+            // 
+            // lstFactorialNumbers
+            // 
+            this.lstFactorialNumbers.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lstFactorialNumbers.Location = new System.Drawing.Point(497, 212);
+            this.lstFactorialNumbers.Name = "lstFactorialNumbers";
+            this.lstFactorialNumbers.Size = new System.Drawing.Size(190, 134);
+            this.lstFactorialNumbers.TabIndex = 7;
+            // 
+            // frmFactorialDoWhle
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lstFactorialNumbers);
+            this.Controls.Add(this.lblAnswer);
+            this.Controls.Add(this.txtNumber);
+            this.Controls.Add(this.lblMultiply);
+            this.Controls.Add(this.BtnStart);
+            this.Controls.Add(this.lblNumber);
+            this.Name = "frmFactorialDoWhle";
+            this.Text = "Factorial By Andrew";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNumber;
+        private System.Windows.Forms.Button BtnStart;
+        private System.Windows.Forms.Label lblMultiply;
+        private System.Windows.Forms.TextBox txtNumber;
+        private System.Windows.Forms.Label lblAnswer;
+        private System.Windows.Forms.ProgressBar lstFactorialNumbers;
+    }
+}
+
---
+++ FactorialDoWhileAndrew/FactorialDoWhileAndrew/FactorialForm.cs
@@ -0,0 +1,70 @@
+﻿/*
+ * Created by: Andrew Kuekam
+ * Created on: 2020-04-04
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #20 - Factorial Do While
+ * This program...
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FactorialDoWhileAndrew
+{
+    public partial class frmFactorialDoWhle : Form
+    {
+        public frmFactorialDoWhle()
+        {
+            InitializeComponent();
+            // Hide lade
+            this.lblAnswer.Hide();
+        }
+
+        private void BtnStart_Click(object sender, EventArgs e)
+        {
+            // Show lbl
+            this.lblAnswer.Show();
+
+            // declare local variables
+            Double factorialAnswer;
+            Double factoriaNumber;
+            int factorialCounter;
+
+            // clear the items fro the listbox
+            this.lstFactorialNumbers.Items.Clear();
+
+            //initialize the final answer to 1
+            factorialAnswer = 1;
+
+            // get the number fromthe user
+            factoriaNumber = Convert.ToDouble(this.txtNumber.Text);
+
+            // set the counter to 0
+            factorialCounter = 0;
+
+            // multiply the country by the next increamented
+            do
+            {
+                // increment the country by 1
+                factorialCounter = factorialCounter + 1;
+
+                // list the counter number in the listbox for the user to see
+                lstFactorialNumbers.Items.Add(factorialCounter);
+
+                // multiply the counter by the answer
+                factorialAnswer = factorialAnswer * factorialCounter;
+
+            } while (factorialCounter < factoriaNumber);
+
+            // convert the factorialAnswer to a String and insert it into the label
+            this.lblAnswer.Text = this.txtNumber.Text + " ! = " + Convert.ToString(factorialAnswer);
+
+        }
+    }
+}
