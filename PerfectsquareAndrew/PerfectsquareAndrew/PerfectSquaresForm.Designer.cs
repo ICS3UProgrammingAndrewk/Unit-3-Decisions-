ae68805a4612e90db68203eae34f46290fe440b4
AndrewK <43419144+AndrewKuekam17@users.noreply.github.com>
2020-04-10 13:02:12 -0400

    Add files via upload

---
+++ PerfectsquareAndrew/PerfectsquareAndrew/PerfectSquaresForm.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace PerfectsquareAndrew
+{
+    partial class frmPerfectSquares
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnStart = new System.Windows.Forms.Button();
+            this.nudNumber = new System.Windows.Forms.NumericUpDown();
+            this.lstPerfertSquares = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.nudNumber)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BtnStart
+            // 
+            this.BtnStart.BackColor = System.Drawing.Color.Blue;
+            this.BtnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnStart.Location = new System.Drawing.Point(131, 81);
+            this.BtnStart.Name = "BtnStart";
+            this.BtnStart.Size = new System.Drawing.Size(155, 38);
+            this.BtnStart.TabIndex = 0;
+            this.BtnStart.Text = "Start";
+            this.BtnStart.UseVisualStyleBackColor = false;
+            this.BtnStart.Click += new System.EventHandler(this.BtnStart_Click);
+            // 
+            // nudNumber
+            // 
+            this.nudNumber.Location = new System.Drawing.Point(131, 160);
+            this.nudNumber.Name = "nudNumber";
+            this.nudNumber.Size = new System.Drawing.Size(155, 20);
+            this.nudNumber.TabIndex = 2;
+            // 
+            // lstPerfertSquares
+            // 
+            this.lstPerfertSquares.FormattingEnabled = true;
+            this.lstPerfertSquares.Location = new System.Drawing.Point(131, 204);
+            this.lstPerfertSquares.Name = "lstPerfertSquares";
+            this.lstPerfertSquares.Size = new System.Drawing.Size(155, 173);
+            this.lstPerfertSquares.TabIndex = 3;
+            // 
+            // frmPerfectSquares
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(388, 425);
+            this.Controls.Add(this.lstPerfertSquares);
+            this.Controls.Add(this.nudNumber);
+            this.Controls.Add(this.BtnStart);
+            this.Name = "frmPerfectSquares";
+            this.Text = "Perfect Squares by Andrew K.";
+            ((System.ComponentModel.ISupportInitialize)(this.nudNumber)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnStart;
+        private System.Windows.Forms.NumericUpDown nudNumber;
+        private System.Windows.Forms.ListBox lstPerfertSquares;
+    }
+}
+
---
+++ PerfectsquareAndrew/PerfectsquareAndrew/PerfectSquaresForm.cs
@@ -0,0 +1,68 @@
+﻿/*
+ * Created by: Andrew Kuekam
+ * Created on: 2020-04-07
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day 22 - Perfect Squares
+ * This program...
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PerfectsquareAndrew
+{
+    public partial class frmPerfectSquares : Form
+    {
+        public frmPerfectSquares()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnStart_Click(object sender, EventArgs e)
+        {
+            // declare variable
+            int endingValue;
+            int value;
+            Double squareRootAsDouble;
+            int squareRootAsInterger;
+
+            // set the initial value to 0
+            value = 0;
+
+            // clear items in the list box
+            this.lstPerfertSquares.Items.Clear();
+
+            // get the user end value selection from the number up_down box
+            endingValue = Convert.ToInt32(this.nudNumber.Value);
+
+            // continue stating any perfect squares between the win and user selection value
+            while (value <= endingValue)
+            {
+                // increment the value by 1
+                value = value + 1;
+
+                // take the square root
+                squareRootAsDouble = Math.Sqrt(value);
+
+                // covert the double to an interger
+                squareRootAsInterger = Convert.ToInt32(squareRootAsDouble);
+
+                // the value has to be perfect square
+                if (squareRootAsInterger == squareRootAsDouble)
+                {
+                    this.lstPerfertSquares.Items.Add(value + " is aperfect square");
+
+                    // refresh from
+                    this.Refresh();
+                }
+            }
+
+        }
+    }
+}
