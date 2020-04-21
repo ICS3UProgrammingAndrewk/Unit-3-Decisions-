313c7f2b21781d18e6cee039f3cef11bae776d91
AndrewK <43419144+AndrewKuekam17@users.noreply.github.com>
2020-04-21 16:06:05 -0400

    Add files via upload

---
+++ UniCodeAndrew/UnicodeForm.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace UniCodeAndrew
+{
+    partial class frmUnicode
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnStart = new System.Windows.Forms.Button();
+            this.lstUnicode = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // BtnStart
+            // 
+            this.BtnStart.BackColor = System.Drawing.Color.Fuchsia;
+            this.BtnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnStart.Location = new System.Drawing.Point(87, 56);
+            this.BtnStart.Name = "BtnStart";
+            this.BtnStart.Size = new System.Drawing.Size(159, 54);
+            this.BtnStart.TabIndex = 0;
+            this.BtnStart.Text = "Start";
+            this.BtnStart.UseVisualStyleBackColor = false;
+            this.BtnStart.Click += new System.EventHandler(this.BtnStart_Click);
+            // 
+            // lstUnicode
+            // 
+            this.lstUnicode.FormattingEnabled = true;
+            this.lstUnicode.Location = new System.Drawing.Point(87, 175);
+            this.lstUnicode.Name = "lstUnicode";
+            this.lstUnicode.Size = new System.Drawing.Size(159, 173);
+            this.lstUnicode.TabIndex = 1;
+            // 
+            // frmUnicode
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(322, 385);
+            this.Controls.Add(this.lstUnicode);
+            this.Controls.Add(this.BtnStart);
+            this.Name = "frmUnicode";
+            this.Text = "Unicode By Andrew K.";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnStart;
+        private System.Windows.Forms.ListBox lstUnicode;
+    }
+}
+
---
+++ UniCodeAndrew/UnicodeForm.cs
@@ -0,0 +1,47 @@
+﻿/*
+ * Created by: Andrew Kuekam
+ * Created on: 2020-04-17
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #24 - Unicode
+ * This program...
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UniCodeAndrew
+{
+    public partial class frmUnicode : Form
+    {
+        public frmUnicode()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnStart_Click(object sender, EventArgs e)
+        {
+            // declare local variables
+            string letters;
+            int letterCounter;
+
+            // Create loop to covert each number
+            for (letterCounter = 65; letterCounter <= 90; letterCounter++)
+            {
+                // convert the counter to unicode letter
+                letters = Char.ConvertFromUtf32(letterCounter);
+
+                // Add letter with its value to the listbox
+                this.lstUnicode.Items.Add(letters + " -> " + letterCounter);
+
+            }
+            // Clear the items in listbox
+            this.lstUnicode.Items.Clear();
+        }
+    }
+}
